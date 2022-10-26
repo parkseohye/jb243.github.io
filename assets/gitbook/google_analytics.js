bc6408578a4f79b3e657dd3982703665eb4187f5
JB243 <55747737+JB243@users.noreply.github.com>
2022-10-26 14:36:28 +0900

    Update and rename note.js to google_analytics.js

---
+++ assets/gitbook/google_analytics.js
@@ -0,0 +1,10 @@
+        <!-- Google Analytics --> 
+        <!-- Google tag (gtag.js) -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-0G7H5JFYB9"></script>
+        <script>
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-0G7H5JFYB9');
+        </script>
